99a6feee9bf15e96bc7b4bb381d4552d24d5ebde
cyrilmuchemi <cyrilmuchemi@gmail.com>
2023-05-26 12:12:19 +0300

    Fetch data from mapbox API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "weather-priary" : "#00668A",
+        "weather-primary" : "#00668A",
         "weather-secondary" : "#004E71"
       }
     },
